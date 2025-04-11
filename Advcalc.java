aa7871f03a6682d59065c4a5f5b53138e8a2d3dd
kikkiredygowthamreddy <gowthamreddykikki6@gmail.com>
2025-04-11 04:40:35 -1200

    Add files via upload

---
+++ Advcalc.java
@@ -0,0 +1,9 @@
+class Advcalc extends calc {
+    public int mul(int n1,int n2)
+    {
+        return n1 * n2;
+    }
+    
+}
+
+
---
+++ good.java
@@ -0,0 +1,10 @@
+
+ 
+public class good{
+    public static void main(String args[]){
+
+Advcalc obj = new Advcalc();
+int r1 = obj.add(3, 7);
+System.out.println(r1);
+    }
+}
